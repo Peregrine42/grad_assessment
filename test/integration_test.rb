bfca85b5a5a7eb317fba227a335be66d9177232d
Duncan Fermor <duncan.fermor@gmail.com>
2015-01-26 22:45:48 +0000

    Duplicate behaviour verified

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -21,14 +21,24 @@ class TestScript < Minitest::Test
     assert_equal(expected_result, actual_result)
   end
 
-  def test_no_table
+  def test_it_finds_the_chains_of_command_between_employees_with_the_same_name
+    chainlink       = Chainlink.new
+
+    expected_result = ["Batman (2) -> Iron Man (9) -> Superman (10) <- Dangermouse (1) <- Invisible Woman (3) <- Super Ted (15)",
+                       "Batman (16) -> Black Widow (6) -> Batman (2) -> Iron Man (9) -> Superman (10) <- Dangermouse (1) <- Invisible Woman (3) <- Super Ted (15)"]
+    actual_result   = chainlink.walk("./fixtures/duplicates.txt", "Batman", "Super Ted")
+
+    assert_equal(expected_result, actual_result)
+  end
+
+  def test_no_table_raises_error
     chainlink       = Chainlink.new
     assert_raises BuilderException do
       result = chainlink.walk("./fixtures/no_table.txt", "Batman", "Super Ted")
     end
   end
 
-  def test_no_header
+  def test_no_header_is_no_problem
     chainlink       = Chainlink.new
 
     expected_result = ["Batman (16) -> Black Widow (6) -> Gonzo the Great (2) -> Iron Man (9) -> Superman (10) <- Dangermouse (1) <- Invisible Woman (3) <- Super Ted (15)"]
