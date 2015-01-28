2e0a9b0dca2ecfac85d6428a5ba3dac81466ca7e
Duncan Fermor <duncan.fermor@gmail.com>
2015-01-28 01:05:58 +0000

    remove redundant functions in walker

--- lib/walker.rb
+++ lib/walker.rb
@@ -4,28 +4,6 @@ class WalkerException < ChainlinkException
 end
 
 class Walker
-  def walk(start_employee, end_employee)
-    path_up, path_down = find_path(start_employee, end_employee)
-    common_manager = path_down.pop
-    path_down.reverse!
-    [path_up, common_manager, path_down]
-  end
-
-  def find_path(start_employee, end_employee, chain = [])
-    possible_path = path_between(end_employee, start_employee.manager)
-    return [chain + [start_employee], possible_path] if possible_path
-    return nil if start_employee.manager.ceo?
-
-    chain << start_employee
-    find_path(start_employee.manager, end_employee, chain)
-  end
-
-  def path_between(employee, manager, chain = [])
-    return chain + [employee, employee.manager] if manager == employee.manager
-    chain << employee
-    return path_between employee.manager, manager, chain unless employee.ceo?
-  end
-
   def compare(emp1, emp2)
     # start from the ceo, working to each employee
     chain1 = emp1.chain_of_command.reverse
--- spec/walker_unit_test.rb
+++ spec/walker_unit_test.rb
@@ -2,39 +2,6 @@ require 'minitest/autorun'
 require './lib/chainlink'
 
 class TestWalker < Minitest::Test
-  def test_path_between
-    manager  = Employee.new(2, 'bob', nil)
-    employee = Employee.new(1, 'bryan', manager)
-
-    actual = Walker.new.path_between(employee, manager)
-    assert_equal([employee, manager], actual)
-  end
-
-  def test_long_path_between
-    manager   = Employee.new(3, 'chris', nil)
-    middleman = Employee.new(2, 'jackie', manager)
-    employee  = Employee.new(1, 'fred', middleman)
-    result = Walker.new.path_between(employee, manager)
-    assert_equal([employee, middleman, manager], result)
-  end
-
-  def test_really_long_path_between
-    ceo       = Employee.new(6, 'bryan', nil)
-    manager   = Employee.new(3, 'sue', ceo)
-    middleman = Employee.new(2, 'ken', manager)
-    employee  = Employee.new(1, 'bob', middleman)
-    result = Walker.new.path_between(employee, ceo)
-    assert_equal([employee, middleman, manager, ceo], result)
-  end
-
-  def test_no_path_between
-    manager   = Employee.new(3, 'emma', nil)
-    middleman = Employee.new(2, 'fred', nil)
-    employee  = Employee.new(1, 'bryan', middleman)
-    result = Walker.new.path_between(employee, manager)
-    assert_equal(nil, result)
-  end
-
   def test_comparing
     ceo        = Employee.new(1, 'ceo', nil)
     manager    = Employee.new(2, 'manager', ceo)
